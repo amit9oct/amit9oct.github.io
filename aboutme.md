87d5ce36045ee6b0638afde5f7a32595f3e7be15
Amitayush Thakur <amit9oct@gmail.com>
2021-08-17 08:24:43 +0530

    Added LaTex in About Me page.

--- aboutme.md
+++ aboutme.md
@@ -1,5 +1,5 @@
 ---
-layout: page
+layout: about
 title: About me
 subtitle: [Coming Soon]
 ---
---
+++ assets/js/latex-setting.js
@@ -0,0 +1,11 @@
+var text = document.getElementById("about-me-latex").innerHTML;
+
+var generator = new latexjs.HtmlGenerator({ hyphenate: false });
+
+generator = latexjs.parse(text, { generator: generator });
+
+// document.head.appendChild(generator.stylesAndScripts("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/"))
+document.head.appendChild(generator.stylesAndScripts(""));
+var aboutMeDiv = document.getElementById("about-me");
+aboutMeDiv.innerHTML = "";
+aboutMeDiv.appendChild(generator.domFragment());
